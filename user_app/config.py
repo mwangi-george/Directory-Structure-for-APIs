389bf6228976227b8751c5fb9d480bcc0c52c4b2
George Mwangi <mwangigeorge648@gmail.com>
2024-07-04 10:32:19 +0300

    Updates

--- user_app/config.py
+++ user_app/config.py
@@ -1,8 +1,9 @@
-""" Config Class goes here"""
+import os
 
 
 class Config:
-    DB_NAME = ...
-    DB_HOST = ...
-    DB_USERNAME = ...
-    DB_PASSWORD = ...
+    DB_NAME = os.getenv("DB_NAME")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_USERNAME = os.getenv("DB_USERNAME")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_PORT = os.getenv("DB_PORT")
